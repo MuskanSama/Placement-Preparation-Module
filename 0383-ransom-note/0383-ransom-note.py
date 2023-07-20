30b832b937ec950f0860b751525bba125e377ff5
MuskanSama <79089453+MuskanSama@users.noreply.github.com>
2023-07-20 12:03:49 +0530

    Time: 84 ms (32.12%), Space: 13.5 MB (50.64%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        ransom_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+
+        for char, count in ransom_count.items():
+            if count > magazine_count[char]:
+                return False
+
+        return True
